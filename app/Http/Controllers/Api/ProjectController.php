1754e43f3229fe2adc81d14bd6918fa70b0b7881
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-07-02 16:37:03 +0200

    feat: created api/show/ route and Api/ProjectController show() function

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -18,4 +18,21 @@ class ProjectController extends Controller
 
         return response()->json($data);
     }
+
+    public function show(string $slug){
+        $project = Project::with(['type','technologies'])->where('slug',$slug)->first();
+
+        if(!$project){
+            return response()->json([
+                'success' => false
+            ],404);
+        }
+
+        $data = [
+            'results' => $project,
+            'success' => true
+        ];
+
+        return response()->json($data);
+    }
 }
